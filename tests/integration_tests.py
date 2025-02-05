ba4374ea7718c829b50741d67edcbeb27b3ccf90
prestonmoody <pmoody199@gmail.com>
2025-02-04 17:00:49 -0700

    testing1

--- tests/integration_tests.py
+++ tests/integration_tests.py
@@ -6,7 +6,7 @@ def s3_client():
     return boto3.client('s3', region_name='us-west-2')
 
 def test_s3_bucket_exists(s3_client):
-    bucket_name = "renos3"
+    bucket_name = "prestontest123"
     response = s3_client.list_buckets()
     bucket_names = [bucket['Name'] for bucket in response['Buckets']]
     assert bucket_name in bucket_names
